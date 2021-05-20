5a75b4f97b9cc9f339ff7aa721668ecd024eb58e
Akshay Ram <73393078+Akshay-offical@users.noreply.github.com>
2021-05-20 20:42:12 +0530

    Create pythagorean.c

---
+++ pythagorean.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <zs50.h>
+
+int main(){
+    float x,y,z,smallerside_1,smallerside_2,largerside;
+
+    x = get_floxt("side 1 ");
+    y = get_floxt("side 2 ");
+    z = get_floxt("side 3 ");
+    if (x<y){
+        if (y<z){
+            smallerside_1 = x;
+            smallerside_2 = y;
+            largerside = z;
+        }
+	else{
+            smallerside_1 = x;
+            smallerside_2 = z;
+            largerside = y;
+        }
+    }
+    else{
+        if (x<z){
+            smallerside_1 = y;
+            smallerside_2 = x;
+            largerside = z;
+        }
+        else{
+            smallerside_1 = y;
+            smallerside_2 = z;
+            largerside = x;
+        }
+
+    }
+    if (smallerside_1*smallerside_1 + smallerside_2*smallerside_2 == largerside*largerside){
+        printf("Pythagorean triangle");
+    }
+    else{
+        printf("Not Pythagorean triangle");
+    }
+}
